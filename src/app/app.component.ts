e6cff601fcf865ccde0ca12bfbb88afbeb4eef09
kaya <jepsonkaya@gmail.com>
2019-06-14 14:49:35 -0700

    add submit button that will update score and level in navbar - currently commented out so app will compile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Navbar } from './models/navbar.model';
 import { NgModule, ViewChild } from '@angular/core';
 import { } from '@types/googlemaps';
@@ -9,30 +9,12 @@ import { } from '@types/googlemaps';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // @Output() sendScore = new EventEmitter();
+  //
+  // submitForm(name: string, price: string, flavor: string, brand: string) {
+  //     let updateGame: Navbar = new Navbar(this.currentFocus.round + 1, this.currentFocus.score, this.currentFocus.map);
+  //     this.sendScore.emit(updateGame);
+  //   }
   title = 'app';
   currentFocus = new Navbar(1,0,"World");
-  // @ViewChild('gmap') gmapElement: any;
-  // map: google.maps.Map;
-  // ngOnInit() {
-  //   var streetViewService = new google.maps.StreetViewService();
-  //   streetViewService.getPanoramaByLocation(
-  //     new google.maps.LatLng(44.0521, -123.0868),
-  //     15,
-  //     function(data, status) {
-  //       console.log(data, status)
-  //     }
-  //   )
-  //   var mapProp = {
-  //     center: new google.maps.LatLng(44.0521, -123.0868),
-  //     zoom: 15,
-  //     mapTypeId: google.maps.MapTypeId.ROADMAP
-  //   };
-  //   this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-  // }
-  // setMapType(mapTypeId: string) {
-  //   this.map.setMapTypeId(mapTypeId)
-  // }
-  // setCenter(e:any){
-  //   this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
-  // }
 }
