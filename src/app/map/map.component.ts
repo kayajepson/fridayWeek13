c076f630bee8a28d0a2adea1c9e3081ff862613e
kaya <jepsonkaya@gmail.com>
2019-06-14 14:08:59 -0700

    add map component successfully but not showing map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit, Output } from '@angular/core';
 import { NgModule, ViewChild } from '@angular/core';
 import { } from '@types/googlemaps';
+import { AgmCoreModule } from '@agm/core';
+import { Navbar } from './models/navbar.model';
+
 
 
 @Component({
-  selector: 'your-comp',
-  styles: ['agm-map { height: 300px; /* height is required */ }'],
-  template: `
-    <agm-map [latitude]='latitude' [longitude]='longitude'
-      [mapTypeId]='mapType'>
-    </agm-map>
-  `
+  selector: 'map-comp',
+  styleUrls: ['./map.component.css'],
+  templateUrl: './map.component.html'
 })
 export class MapComponent {
-  latitude = -28.68352;
-  longitude = -147.20785;
-  mapType = 'satellite';
+  ngOnInit() {
+    var streetViewService = new google.maps.StreetViewService();
+    streetViewService.getPanoramaByLocation(
+      new google.maps.LatLng(44.0521, -123.0868),
+      15,
+      function(data, status) {
+        console.log(data, status)
+      }
+    )
+    var mapProp = {
+      center: new google.maps.LatLng(44.0521, -123.0868),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+  setCenter(e:any){
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+  }
 }
