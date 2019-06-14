cbc3c3d92452d5adc55f2a25470421e054787b76
kaya <jepsonkaya@gmail.com>
2019-06-14 11:54:10 -0700

    troubles with streetview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,65 +17,48 @@ export class AppComponent {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
 
-//   ngOnInit() {
-//     var mapProp = {
-//       center: new google.maps.LatLng(44.0521, -123.0868),
-//       zoom: 15,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//     };
-//     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-//   }
-//   setMapType(mapTypeId: string) {
-//     this.map.setMapTypeId(mapTypeId)
-//   }
-//   // setCenter(e:any){
-//   //   this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
-//   // }
-// }
+  ngOnInit() {
 
-ngOnInit() {
-  var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), {
-        position: {lat: 37.869, lng: -122.255},
-        pov: {
-          heading: 270,
-          pitch: 0
-        },
-        visible: true
-  });
+    var geocoder = new google.maps.Geocoder();
+    var address = "2 Simei Street 3, Singapore, Singapore 529889";
 
-  panorama.addListener('pano_changed', function() {
-      var panoCell = document.getElementById('pano-cell');
-      panoCell.innerHTML = panorama.getPano();
-  });
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
 
-  panorama.addListener('links_changed', function() {
-      var linksTable = document.getElementById('links_table');
-      while (linksTable.hasChildNodes()) {
-        linksTable.removeChild(linksTable.lastChild);
-      }
-      var links = panorama.getLinks();
-      for (var i in links) {
-        var row = document.createElement('tr');
-        linksTable.appendChild(row);
-        var labelCell = document.createElement('td');
-        labelCell.innerHTML = '<b>Link: ' + i + '</b>';
-        var valueCell = document.createElement('td');
-        valueCell.innerHTML = links[i].description;
-        linksTable.appendChild(labelCell);
-        linksTable.appendChild(valueCell);
-      }
-  });
+            console.log(latitude + " " + longitude);
 
-  panorama.addListener('position_changed', function() {
-      var positionCell = document.getElementById('position-cell');
-      positionCell.firstChild.nodeValue = panorama.getPosition() + '';
-  });
+            var svService = new google.maps.StreetViewService();
+            var panoRequest = {
+                location: results[0].geometry.location,
+                preference: google.maps.StreetViewPreference.NEAREST,
+                radius: 50,
+                source: google.maps.StreetViewSource.OUTDOOR
+            };
 
-  panorama.addListener('pov_changed', function() {
-      var headingCell = document.getElementById('heading-cell');
-      var pitchCell = document.getElementById('pitch-cell');
-      headingCell.firstChild.nodeValue = panorama.getPov().heading + '';
-      pitchCell.firstChild.nodeValue = panorama.getPov().pitch + '';
-  });
+            var findPanorama = function(radius) {
+                panoRequest.radius = radius;
+                svService.getPanorama(panoRequest, function(panoData, status){
+                    if (status === google.maps.StreetViewStatus.OK) {
+                        var panorama = new google.maps.StreetViewPanorama(
+                            document.getElementById('street-view'),
+                            {
+                                pano: panoData.location.pano,
+                            });
+                    } else {
+                        //Handle other statuses here
+                        if (radius > 200) {
+                            alert("Street View is not available");
+                        } else {
+                            findPanorama(radius + 5);
+                        }
+                    }
+                });
+            };
+
+            findPanorama(50);
+        }
+    });
+}
 }
