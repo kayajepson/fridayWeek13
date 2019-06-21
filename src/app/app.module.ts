2ecb7053a15b3d1b818e2957bc6e81bf1d2589a6
kaya <jepsonkaya@gmail.com>
2019-06-21 15:13:58 -0700

    set up firebase and create database json file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,17 @@ import { NavBarComponent } from './navbar/navbar.component';
 import { MapComponent } from './map/map.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 
 @NgModule({
@@ -18,7 +29,9 @@ import { routing } from './app.routing';
   imports: [
     BrowserModule,
       HttpModule,
-      routing
+      routing,
+      AngularFireModule.initializeApp(firebaseConfig),
+AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
