19664be160383caa0e8b4e5dca50c94dd6fa5bcc
kaya <jepsonkaya@gmail.com>
2019-06-22 11:32:50 -0700

    firebase returning data

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Map } from '../models/map.model'
 import { FirebaseListObservable } from 'angularfire2/database';
 import { MapService } from '../map.service';
 import { Router } from '@angular/router';
@@ -9,14 +10,15 @@ import { Router } from '@angular/router';
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css'],
-    providers: [MapService]
+  providers: [MapService]
 })
 export class WelcomeComponent implements OnInit {
   maps: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private mapService: MapService) { }
 
   ngOnInit() {
+    this.maps = this.mapService.getMaps();
   }
 
 
