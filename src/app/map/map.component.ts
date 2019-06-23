52bfebe92fedc9a7992936ba235c3160199f2c15
kaya <jepsonkaya@gmail.com>
2019-06-23 10:17:45 -0700

    add marker and click listeners to display or clear markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -31,8 +31,10 @@ export class MapComponent {
     }
 
     initMiniMap() {
-      var myLatLng = {lat: 14.599, lng: 28.673};
-
+      let myLatLng = {lat: 14.599, lng: 28.673};
+      let latGuess;
+      let longGuess;
+      let markers = [];
       var map = new google.maps.Map(document.getElementById('small-map'), {
         zoom: 1,
         center: myLatLng
@@ -42,12 +44,43 @@ export class MapComponent {
         placeMarker(event.latLng);
       });
 
+
       function placeMarker(location) {
+        clearMarkers();
         var marker = new google.maps.Marker({
           position: location,
           map: map
         });
+        markers.push(marker);
+        latGuess = marker.getPosition().lat();
+        longGuess = marker.getPosition().lng();
+        console.log("hello", latGuess);
       }
+
+      // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }
+
+
     }
 
   }
